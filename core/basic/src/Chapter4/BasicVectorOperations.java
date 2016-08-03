04c36ca1fc8d5853559ee52ecf1a13d3849bfdad
papu12 <papubhat@gmail.com>
2016-08-03 09:36:31 +0530

    Adding 6 collection program

---
+++ core/basic/src/Chapter4/BasicVectorOperations.java
@@ -0,0 +1,53 @@
+/*
+ *
+ *  * See the NOTICE file distributed with this work for additional
+ *  * information regarding copyright ownership.
+ *  *
+ *  * This is free software; you can redistribute it and/or modify it
+ *  * under the terms of the GNU Lesser General Public License as
+ *  * published by the Free Software Foundation; either version 2.1 of
+ *  * the License, or (at your option) any later version.
+ *  *
+ *  * This software is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  * Lesser General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU Lesser General Public
+ *  * License along with this software; if not, write to the Free
+ *  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ *
+ */
+
+package Chapter4;
+
+/**
+ * Created by papu bhattacharya on 3/8/16.
+ */
+
+import java.util.Vector;
+
+public class BasicVectorOperations {
+
+    public static void main(String a[]) {
+        Vector<String> vct = new Vector<String>();
+        //adding elements to the end
+        vct.add("First");
+        vct.add("Second");
+        vct.add("Third");
+        System.out.println(vct);
+        //adding element at specified index
+        vct.add(2, "Random");
+        System.out.println(vct);
+        //getting elements by index
+        System.out.println("Element at index 3 is: " + vct.get(3));
+        //getting first element
+        System.out.println("The first element of this vector is: " + vct.firstElement());
+        //getting last element
+        System.out.println("The last element of this vector is: " + vct.lastElement());
+        //how to check vector is empty or not
+        System.out.println("Is this vector empty? " + vct.isEmpty());
+    }
+}
---
+++ core/basic/src/Chapter4/MyEnumeration.java
@@ -0,0 +1,48 @@
+/*
+ *
+ *  * See the NOTICE file distributed with this work for additional
+ *  * information regarding copyright ownership.
+ *  *
+ *  * This is free software; you can redistribute it and/or modify it
+ *  * under the terms of the GNU Lesser General Public License as
+ *  * published by the Free Software Foundation; either version 2.1 of
+ *  * the License, or (at your option) any later version.
+ *  *
+ *  * This software is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  * Lesser General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU Lesser General Public
+ *  * License along with this software; if not, write to the Free
+ *  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ *
+ */
+
+package Chapter4;
+
+/**
+ * Created by papu bhattacharya on 3/8/16.
+ */
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class MyEnumeration {
+    public static void main(String a[]) {
+        Vector<String> lang = new Vector<String>();
+        Enumeration<String> en = null;
+        lang.add("JAVA");
+        lang.add("JSP");
+        lang.add("SERVLET");
+        lang.add("EJB");
+        lang.add("PHP");
+        lang.add("PERL");
+        en = lang.elements();
+        while (en.hasMoreElements()) {
+            System.out.println(en.nextElement());
+        }
+    }
+}
